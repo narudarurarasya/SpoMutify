9f7d15b68991da925c904ca52cc0cace5286e2fe
Vivek Vaddina <vaddinavivek@gmail.com>
2016-03-06 02:58:36 +0100

    First commit - Added Script & Readme section...

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SpoMutify
-Tiny script to mute ads in Spotify free (Desktop) version
+
+A puny AppleScript (& bash) that mutes Spotify player during ads and gracefully restores the original volume when the songs are back.
+
+# Pre-requisites:
+Runs only on Desktop App of Mac OS X (since it uses Dictionary of the Spotify App - No API permissions, nada...)
+
+# References:
+1. https://jamfnation.jamfsoftware.com/discussion.html?id=5541
+2. http://broadcast.oreilly.com/2009/05/itunes-dj-applescript-fade-to.html
+
+Make it executable and run it in background. `($ ./MuteSpotify.sh &)`
---
+++ SpoMutify.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Keep on checking (forever) whether Spotify is running...
+while :
+do
+
+	# Process check to see if Spotify is running...
+	if ps aux | grep -v grep | grep Spotify.app
+	then
+
+	# Specify to run it as an OS applesctipt specifying it ends at EOT
+	/usr/bin/osascript <<EOT
+
+
+	-- set the avg. duration of an ad (in ms). Default (60000 ms == 60 sec.)
+	set ad_duration to 60000
+
+	-- set the wait interval (in seconds) for constantly checking for ads in loop (Default 5s)
+	set delay_timer to 5
+
+	repeat until application "Spotify" is not running
+
+		-- Boolean switch to toggle when an ad shows up
+		set is_ad to "No"
+
+		tell application "Spotify"
+
+			-- Get the volume of Spotify player (Not system Volume)
+			set originalVolume to get sound volume
+			set trackTime to get duration of current track
+
+			if trackTime ≤ ad_duration then
+				set is_ad to "yes"
+				-- delay delay_timer
+				-- display notification "ad duration: " & trackTime & ", Vol: " & currentVolume & " -> 10 "
+
+				-- Fade volume gracefully
+				set currentVolume to get sound volume
+				-- display notification "before reducing: " & currentVolume
+				if currentVolume is not 0 then
+					repeat with i from currentVolume to 1 by -1 -- Adjust to taste; bigger numbers fade faster
+						set sound volume to i
+						delay 0.02 -- Adjust this to change fadeout duration (delete this line on slower Macs)
+					end repeat
+				end if
+
+				-- display notification "after reducing: " & sound volume
+			end if
+
+			-- Check periodically whether the ads are still playing...
+			repeat while trackTime ≤ ad_duration
+				-- display notification "Ad going on... delaying " & delay_timer & " secs. "
+				delay delay_timer
+				set trackTime to get duration of current track
+			end repeat
+
+			if is_ad is "yes" then
+				set currentVolume to get sound volume
+
+				-- display notification "Before increasing: " & currentVolume
+				if currentVolume = 0 then
+					-- Increase Volume gracefully
+					repeat with i from currentVolume to (originalVolume + 1) by 1
+						set sound volume to i
+						delay 0.02
+					end repeat
+				end if
+				-- display notification "After increasing: " & sound volume
+			end if
+		end tell
+
+		delay delay_timer
+	end repeat
+
+EOT
+fi
+# sleep for 5 seconds and check again...
+sleep 5
+done
+exit 0
